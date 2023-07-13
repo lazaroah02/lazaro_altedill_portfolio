8a4a332736ac9273bb10560a0d47f74fdce651cf
lazaroah <lazaroah02@gmail.com>
2023-07-13 14:27:54 -0400

    skills pages finished

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,6 +1,17 @@
+import NavBar from '../../components/NavBar'
+import ContactButtons from '../../components/ContactButtons'
+import '../general-pages-styles.css'
+import './index.css'
+
 function About() {
     return ( 
-        <div>About</div>
+        <div className = "page-background">
+            <NavBar/>
+            <section className = "about-content-container page-content-panel">
+
+            </section>
+            <ContactButtons/>
+        </div>
      );
 }
 
--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -1,6 +1,22 @@
+import NavBar from '../../components/NavBar'
+import ContactButtons from '../../components/ContactButtons'
+import { SKILLS } from '../../assets/skills/skills.js'
+import './index.css'
+import '../general-pages-styles.css'
+
 function Skills() {
     return ( 
-        <div>Skills</div>
+        <div className = "page-background">
+            <NavBar/>
+            <section className = "page-content-panel skills-content-container">
+                {SKILLS.map(skill => 
+                    <div onClick={() => setRotedCard(!rotedCard)}>
+                        <img alt = {skill.name} src = {skill.url}/>
+                    </div>
+                )}
+            </section>
+            <ContactButtons/>
+        </div>
      );
 }
 
