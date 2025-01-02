cee9240e0c994771dcc9737456a47e8ba3d01f01
Lazaro Altedill <lazaroah02@gmail.com>
2025-01-01 20:23:54 -0500

    new position for nav-bar, new animation for nav-bar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -7,21 +7,25 @@ function NavBar() {
         <a href = "#home">
           Home
         </a>
+        <span></span>
       </div>
       <div className="navbar-item">
         <a href = "#about">
           About
         </a>
+        <span></span>
       </div>
       <div className="navbar-item item-large">
         <a href = "#skills">
           Skills
         </a>
+        <span></span>
       </div>
       <div className="navbar-item">
         <a href = "#projects">
           Projects
         </a>
+        <span></span>
       </div>
     </div>
   );
