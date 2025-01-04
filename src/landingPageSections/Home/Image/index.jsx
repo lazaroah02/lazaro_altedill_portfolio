aa36484869af8aa8db9ee92a701a19063a3a8f84
Lazaro Altedill <lazaroah02@gmail.com>
2025-01-04 09:10:47 -0500

    adjusting skill cards animation

--- src/landingPageSections/Home/Image/index.jsx
+++ src/landingPageSections/Home/Image/index.jsx
@@ -1,7 +1,22 @@
 import MyImage from '@/assets/my-image-homepage.webp'
 import './index.css'
+import { useEffect, useState } from 'react';
 
 export function HomePageImage() {
+    const [mousePosition, setMousePosition] = useState({x:0, y:0})
+    
+    useEffect(() => {
+        const homePageReference = document.getElementById('home')
+        homePageReference.addEventListener('mousemove', (e) => {
+            setMousePosition({x:e.clientX, y:e.clientY})
+        })
+        animateLuminousEffect()
+    },[])
+
+    function animateLuminousEffect(){
+        window.requestAnimationFrame(animateLuminousEffect)
+    }
+
     return ( 
         <section className = "hompage-image-container">
             <img className = "my-homepage-image" src = {MyImage.src}/>
