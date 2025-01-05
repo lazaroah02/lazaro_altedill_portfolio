55081ae609b2a944e34c84442a6e1f73af821c5b
Lazaro Altedill <lazaroah02@gmail.com>
2025-01-05 13:09:55 -0500

    projects and footer rebranding finished

--- src/landingPageSections/Footer/index.jsx
+++ src/landingPageSections/Footer/index.jsx
@@ -1,13 +1,9 @@
 import "./index.css"
-import ContactButtons from "@/components/ContactButtons";
 
 function Footer({id}) {
     return ( 
         <section className = "footer-section" id = {id}>
-            <div className = "footer-contact-buttons-container">
-                <ContactButtons/>
-                <div className = "footer-phrase">{"<Build the Future with your code/>"}</div>
-            </div>
+            <div className = "footer-phrase">{"<Build the Future with your code/>"}</div>
         </section>
      );
 }
