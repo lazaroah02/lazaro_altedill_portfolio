58fe9cfeddfb81acdc154d9326d9f30401f36c40
Lazaro Altedill <lazaroah02@gmail.com>
2025-01-05 12:21:23 -0500

    button-got-top animation added

--- src/components/ButtonGoTop/index.jsx
+++ src/components/ButtonGoTop/index.jsx
@@ -1,30 +1,41 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './index.css'
 import UpArrow from '@/assets/icons/up-arrow-black.svg'
 
 export default function ButtonGoTop(){
-    const [showButtonScrollTop, setShowButtonScrollTop] = useState(false)
+    const [showButton, setShowButton] = useState(false)
+    const [showHideAnimation, setShowHideAnimation] = useState(false)
 
     //function that focus on the start of the page
     function goTop(){
         window.scrollTo({top:0, left:0, behavior:"smooth"})
     }
+
     //function that detect when scroll is higher than x and show the button go top
-    window.onscroll = function(){
+    function showOrHideButton(){
         if(window.scrollY >= 100){
-        setShowButtonScrollTop(true)
+            setShowHideAnimation(false)
+            setShowButton(true)
         }
         else{
-        setShowButtonScrollTop(false)
+            setShowHideAnimation(true)
         }
     }
-    return(
+
+    useEffect(() => {
+        //first check to show the button on page reload, when still the user hasn't made any scroll
+        window.scrollY >= 100? setShowButton(true): setShowButton(false)
+        window.addEventListener("scroll", showOrHideButton)
+        return () => {
+            window.removeEventListener("scroll", showOrHideButton)
+        }
+    },[])
+
+    return(showButton?
         <div>
-            {showButtonScrollTop?
-            <button className = 'button-scroll-top' onClick={() => goTop()}>
+            <button className = {`button-scroll-top ${showHideAnimation?'hide':''}`} onClick={() => goTop()}>
                 <img alt = {"up arrow"} src = {UpArrow.src}/>
             </button>
-            :null}
-        </div>
+        </div>:null
     )
 }
